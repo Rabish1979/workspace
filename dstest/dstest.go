40bb3e37c2e8b9fc66e379f749e8ca7bdfa7ceaf
Rabish Kumar <krabish@vmware.com>
2024-07-27 10:04:04 +0530

    fixes.

--- dstest/dstest.go
+++ dstest/dstest.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Rabish1979/workspace/linklist"
 )
 
 func main() {
-	myPlaylist := createLinkList()
-	myPlaylist.insertAtFront(20)
-	myPlaylist.insertAtFront(30)
-	myPlaylist.insertAtFront(40)
-	myPlaylist.Print()
+	var myPlaylist linklist.LinkList
+	fmt.Printf("myPlaylist2: %v\n", myPlaylist)
 	fmt.Println("hello ds")
 }
--- dstest/go.mod
+++ dstest/go.mod
@@ -2,4 +2,4 @@ module github.com/Rabish1979/workspace/dstest
 
 go 1.21.3
 
-require github.com/Rabish1979/workspace/linklist v0.0.0-20240727034718-62137211dddd // indirect
+require github.com/Rabish1979/workspace/linklist v0.0.0-20240727041122-792927d10ccd // indirect
