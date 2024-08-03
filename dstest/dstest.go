be1de0bcb7a6245bd42f4c87fcff98544c65037b
Rabish Kumar <krabish@vmware.com>
2024-08-03 22:03:09 +0530

    hashmap usage.

--- dstest/dstest.go
+++ dstest/dstest.go
@@ -24,4 +24,33 @@ func main() {
 	myTree.Insert(25)
 	myTree.Insert(30)
 	myTree.InOrder()
+
+	TestHashMap()
+}
+
+func TestHashMap() {
+	hashmap := make(map[string]int)
+
+	// Add some items to the map
+	hashmap["pencil"] = 10
+	hashmap["pen"] = 20
+	hashmap["scale"] = 15
+
+	// Print the entire map
+	fmt.Println("The hashmap created above is: ")
+	fmt.Println(hashmap)
+
+	// Access a specific item by its key
+	fmt.Println("The value of specific element from hashmap is:")
+	fmt.Println(hashmap["pencil"])
+
+	// Update the value of an existing item
+	hashmap["scale"] = 20
+
+	// Delete an item from the map
+	delete(hashmap, "pencil")
+
+	// Print the updated map
+	fmt.Println("The hashmap after deleting an item from it is:")
+	fmt.Println(hashmap)
 }
